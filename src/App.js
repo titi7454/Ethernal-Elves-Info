3622e848f42f58f97e2ad1a59ac3c9d4442e44ba
titi7454 <konstantin19961996@abv.bg>
2022-11-29 10:41:22 +0100

    Deployed website

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import LookUp from "./LookUp";
 import Analytics from "./Analytics";
 import NavBar from "./NavBar";
 import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <div className="App-header">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Analytics />}/>
+        <Route path="/Ethernal-Elves-Info" element={<Analytics />}/>
         <Route path="/LookUp" element={<LookUp />}/>
+        <Route path="/Dashboard" element={<Dashboard />}/>
       </Routes>
     </div>
   );
