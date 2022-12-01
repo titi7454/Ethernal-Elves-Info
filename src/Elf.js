b311cb6c3a84267d07035997678985cd963299a7
titi7454 <konstantin19961996@abv.bg>
2022-12-01 22:40:20 +0100

    Cooldown fix

--- src/Elf.js
+++ src/Elf.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-import Countdown from 'react-countdown';
+import Countdown from "react-countdown";
 
 const Elf = ({ elfId, collection }) => {
   const api = `https://api.ethernalelves.com/api/${collection}/${elfId}`;
@@ -8,34 +8,41 @@ const Elf = ({ elfId, collection }) => {
 
   const a = (n) => {
     return (
-        <div>
-          {elfData.attributes[n].trait_type}:{elfData.attributes[n].value}
-        </div>
+      <div>
+        {elfData.attributes[n].trait_type}:{elfData.attributes[n].value}
+      </div>
     );
   };
 
   const getCurrUnix = () => {
-    return Math.floor((Date.now() / 1000) + 3600)
-  }
+    return Math.floor(Date.now() / 1000 + 3600);
+  };
 
-  const isElder = () =>{
-    if(elfData.attributes[8].trait_type === "Agility"){
-       const isZero = elfData.attributes[14].value < getCurrUnix() ? "Ready" : getCD(elfData.attributes[14].value-getCurrUnix())
-       return isZero
+  const isElder = () => {
+    if (elfData.attributes[8].trait_type === "Agility") {
+      const isZero =
+        elfData.attributes[14].value < getCurrUnix()
+          ? "Ready"
+          : getCD(elfData.attributes[14].value);
+      return isZero;
     } else {
-        return getCD(elfData.attributes[8].value)
+      const isZero =
+        elfData.attributes[8].value < getCurrUnix()
+          ? "Ready"
+          : getCD(elfData.attributes[8].value);
+      return isZero;
     }
-  }
+  };
 
   const getCD = (unix) => {
-    let timestamp = new Date(unix*1000);
+    let timestamp = new Date(unix * 1000);
     // var date = new Date(timestamp * 1000);
     // // Hours part from the timestamp
     // var h = date.getHours();
     // var hours = h < 10 ? "0" + date.getHours() : date.getHours();
     // // Minutes part from the timestamp
     // var m = date.getMinutes();
-    // var minutes = m < 10 ? "0" + date.getMinutes() : date.getMinutes(); 
+    // var minutes = m < 10 ? "0" + date.getMinutes() : date.getMinutes();
     // // Seconds part from the timestamp
     // var s = date.getSeconds();
     // var seconds = s < 10 ? "0" + date.getSeconds() : date.getSeconds();
@@ -43,9 +50,7 @@ const Elf = ({ elfId, collection }) => {
     // // Will display time in 10:30:23 format
     // var formattedTime =
     //   hours + ":" + minutes + ":" + seconds;
-      return (
-        <Countdown date={timestamp} />
-      )
+    return <Countdown date={timestamp} />;
   };
   useEffect(() => {
     //Get data from the api
@@ -65,11 +70,7 @@ const Elf = ({ elfId, collection }) => {
       <div className="box-inner">
         <div>{elfData.name}</div>
         <div>
-          <img
-            className="responsive"
-            src={elfData.image}
-            alt="elf"
-          ></img>
+          <img className="responsive" src={elfData.image} alt="elf"></img>
         </div>
         <div>{a(0)}</div>
         <div>{a(2)}</div>
