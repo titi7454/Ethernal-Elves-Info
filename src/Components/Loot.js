eb4961cc79e38771d7b056f38678d068e3900f61
titi7454 <konstantin19961996@abv.bg>
2023-05-07 02:42:58 +0200

    vercel

--- src/Components/Loot.js
+++ src/Components/Loot.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import REN from "../Assets/Ren.png";
-import MOON from "../Assets/Moon.png";
 import Aether from "../Assets/Aether.png";
 import Artifacts from "../Assets/Artifacts.png";
 import Scrolls from "../Assets/Scrolls.png";
@@ -36,9 +35,7 @@ const Loot = ({ lootData }) => {
           {lootData[0].name}
           <div className="lootItems">
             {lootItems(1, REN)}
-            {lootItems(2, MOON)}
             {lootItems(3, ZUG)}
-            {lootItems(4, MOON)}
           </div>
         </div>
         <div>
